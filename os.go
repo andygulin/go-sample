2881a634fc86aaa92c4c13113d3a6043c2b0a56a
andygulin <andygulin@hotmail.com>
2018-01-03 15:25:50 +0800

    ...

---
+++ os.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Hostname())
+	fmt.Println(os.Getpid())
+	fmt.Println(os.TempDir())
+	envs := os.Environ()
+	for _,env := range envs {
+		fmt.Println(env)
+	}
+}
