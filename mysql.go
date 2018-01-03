32626394229851115c2e380ff4c93f3a8e7e5b43
andygulin <andygulin@hotmail.com>
2018-01-03 15:05:17 +0800

    ...

---
+++ mysql.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+	"fmt"
+)
+
+/**
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ */
+
+func main() {
+	db, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/test?charset=utf8")
+	defer db.Close()
+
+	stmt, _ := db.Prepare("INSERT INTO user VALUES(NULL,?,?,?,?)")
+	stmt.Exec("呵呵", 11, "上海", time.Now())
+	stmt.Close()
+
+	row := db.QueryRow("SELECT * FROM user ORDER BY id DESC LIMIT 1")
+	var id, age int
+	var name, address string
+	var createdAt time.Time
+	row.Scan(&id, &name, &age, &address, &createdAt)
+	fmt.Println(id, name, age, address, createdAt)
+}
